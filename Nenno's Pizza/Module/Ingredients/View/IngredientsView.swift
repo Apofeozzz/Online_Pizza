d58a658398ddc5c92ab5c2e66cdc4aec5240914d
DenisGrishchenkoIOS <denis.grishchenko.ios@gmail.com>
2020-02-25 00:23:17 +0200

    Ingredients View started.

--- Nenno's Pizza/Module/Ingredients/View/IngredientsView.swift
+++ Nenno's Pizza/Module/Ingredients/View/IngredientsView.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2020 DenisGrishchenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class IngredientsView: UIView {
+	
+}
--- Nenno's Pizza/Module/Ingredients/View/IngredientsViewController.swift
+++ Nenno's Pizza/Module/Ingredients/View/IngredientsViewController.swift
@@ -8,4 +8,54 @@
 
 import UIKit
 
-
+class IngredientsViewController: UIViewController {
+	
+	// MARK: - UIVIEW -
+	
+	var mainView: IngredientsView!
+	
+	// MARK: - LIFE CYCLE -
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupView()
+		
+	}
+	
+	// MARK: - SETUP VIEW -
+    
+    private func setupView() {
+        
+        view.backgroundColor = .white
+        
+        setupMainView()
+        
+        setupConstraints()
+        
+    }
+    
+    private func setupMainView() {
+        
+        mainView = IngredientsView()
+        
+        view.addSubview(mainView)
+        
+    }
+    
+    // MARK: - SETUP CONSTRAINTS -
+    
+    private func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+        
+            mainView.topAnchor      .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainView.leadingAnchor  .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainView.trailingAnchor .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainView.bottomAnchor   .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        
+        ])
+        
+    }
+	
+}
